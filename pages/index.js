034098a4fb1ee799d1d891f06ce956957fe2b400
lautarovilas <vilaslautaro@gmail.com>
2022-07-11 22:21:14 -0300

    add meta tags

--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,43 @@ import { Box, Center, Image, Text } from "@chakra-ui/react";
 
 export default function HomePage() {
   return (
-    <Center
-      display={"flex"}
-      flexDirection="column"
-      alignItems={"center"}
-      marginTop={{base: "20vh", md: "5vh"}}
-      paddingX={"10px"}
-    >
-      <Center fontSize={{ base: "25px", md: "50px" }} mb={10} textAlign="center">
-        ¡Sitio web en construccion!
+    <>
+      <Head>
+        <title>{"Agencia de marketing crypto - CryptoLabs"}</title>
+        <meta name="description" content={`Somos una agencia de marketing crypto.
+         Brindamos servicios de Marketing, publicidad, consultoria y muchos más, para lograr que su empresa tenga usuarios, reconocimiento
+   y prestigio en el ecosistema crypto.`} />
+      </Head>
+      <Center
+        display={"flex"}
+        flexDirection="column"
+        alignItems={"center"}
+        marginTop={{ base: "20vh", md: "5vh" }}
+        paddingX={"10px"}
+      >
+        <Center
+          fontSize={{ base: "25px", md: "50px" }}
+          mb={10}
+          textAlign="center"
+        >
+          ¡Sitio web en construccion!
+        </Center>
+        <Box>
+          <Text fontSize={{ base: "20px", md: "25px" }}>
+            Muy pronto habra novedades!
+          </Text>
+          <Center fontSize={{ base: "20px", md: "25px" }}>By Cryptolabs</Center>
+        </Box>
+        <Image
+          src={
+            "https://res.cloudinary.com/dn7qsxzdf/image/upload/v1657579487/cryptolabs/Under_Constructions_Flatline_a10qpc.png"
+          }
+          alt={"Sitio web en construccion"}
+          width={{ base: 300, md: 700 }}
+          height={{ base: 300, md: 700 }}
+        />
       </Center>
-      <Box>
-        <Text fontSize={{ base: "20px", md: "25px" }}>
-          Muy pronto habra novedades!
-        </Text>
-        <Center fontSize={{ base: "20px", md: "25px" }}>By Cryptolabs</Center>
-      </Box>
-      <Image
-        src={
-          "https://res.cloudinary.com/dn7qsxzdf/image/upload/v1657579487/cryptolabs/Under_Constructions_Flatline_a10qpc.png"
-        }
-        alt={"Sitio web en construccion"}
-        width={{ base: 300, md: 700 }}
-        height={{ base: 300, md: 700 }}
-      />
-    </Center>
+    </>
   );
 }
 
